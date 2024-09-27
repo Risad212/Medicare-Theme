1db8ae482f252a41e5eb1f8df44d90bbec6cf526
Risad212 <hafez.risad@gmail.com>
2024-09-27 11:36:30 +0600

    about mission done

--- template-parts/about/about-hospital.php
+++ template-parts/about/about-hospital.php
@@ -16,23 +16,23 @@ $about_btn_link = esc_url($options['about-page-btn']['url']);
             <div class="col-lg-6">
                 <div class="about-image">
                     <div class="circle">
-                        <img src="<?php echo $about_page_circle_image; ?>" alt="Circle Image">
+                        <img src="<?php echo esc_url($about_page_circle_image); ?>" alt="Circle Image">
                     </div>
                     <div class="first-img">
-                        <img class="img-fluid" src="<?php echo $about_page_small_image; ?>" alt="Small Image">
+                        <img class="img-fluid" src="<?php echo esc_url( $about_page_small_image) ?>" alt="Small Image">
                     </div>
                     <div class="second-img">
-                        <img class="img-fluid" src="<?php echo $about_page_large_image; ?>" alt="Large Image">
+                        <img class="img-fluid" src="<?php echo esc_url( $about_page_large_image) ?>" alt="Large Image">
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="about-content">
-                    <h6 class="subtitle"><?php echo $about_page_subtitle; ?></h6>
-                    <h2 class="title"><?php echo $about_page_title; ?></h2>
-                    <p><?php echo $about_page_disc; ?></p>
+                    <h6 class="subtitle"><?php echo esc_html($about_page_subtitle); ?></h6>
+                    <h2 class="title"><?php echo esc_html($about_page_title); ?></h2>
+                    <p><?php echo esc_html($about_page_disc); ?></p>
                     <button class="about-btn">
-                        <a href="<?php echo $about_btn_link; ?>"><?php echo $about_btn_text; ?></a>
+                        <a href="<?php echo esc_url( $about_btn_link); ?>"><?php echo esc_html($about_btn_text); ?></a>
                         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" viewBox="0 0 512 512" class="">
                             <g>
                                 <path d="m506.134 241.843-.018-.019-104.504-104c-7.829-7.791-20.492-7.762-28.285.068-7.792 7.829-7.762 20.492.067 28.284L443.558 236H20c-11.046 0-20 8.954-20 20s8.954 20 20 20h423.557l-70.162 69.824c-7.829 7.792-7.859 20.455-.067 28.284 7.793 7.831 20.457 7.858 28.285.068l104.504-104 .018-.019c7.833-7.818 7.808-20.522-.001-28.314z" class=""></path>
--- template-parts/about/about-mission.php
+++ template-parts/about/about-mission.php
@@ -1,34 +1,35 @@
 <?php 
 $options = get_option('medicare-options'); 
-$about_mission = isset($options['about-mission']) ? $options['about-mission'] : [];
+$about_mission = $options['about-mission'];
 ?>
 
-<section class="our-vision">
-    <div class="container">
-        <div class="row">
-            <?php 
-            foreach($about_mission as $card) {
-                $title = esc_html($card['about-mission-title']);
-                $description = esc_html($card['about-mission-disc']);
-            ?>
-                <div class="col-lg-4">
-                    <div class="single-vision-box">
+  <section class="our-vission">
+        <div class="container">
+            <div class="row">
+                <?php
+                  foreach($about_mission as $card){
+                    $title = esc_html($card['about-mission-title']);
+                    $disc = esc_html($card['about-mission-disc']);
+                 ?>
+                   <div class="col-lg-4">
+                    <div class="single-vission-box">
                         <div class="icon">
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                                 <g id="SVGRepo_iconCarrier">
-                                    <path d="M4.89163 13.2687L9.16582 17.5427L18.7085 8" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M4.89163 13.2687L9.16582 17.5427L18.7085 8" stroke-width="2.5"
+                                        stroke-linecap="round" stroke-linejoin="round"></path>
                                 </g>
                             </svg>
                         </div>
-                        <h4 class="title"><?php echo $title; ?></h4>
-                        <p><?php echo $description; ?></p>
+                        <h4 class="title"><?php echo $title ?></h4>
+                        <p><?php echo $disc ?></p>
                     </div>
                 </div>
-            <?php
-            }
-            ?>
+                 <?php
+                  }
+                 ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
